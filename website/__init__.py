f91d1a048a001e4561f02f2445c49172f97eb900
Sahil <84409956+sahilsasane@users.noreply.github.com>
2023-10-19 02:35:25 -0700

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -10,7 +10,8 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://sahilsasane:BWR1XoxgIy8e
+@ep-odd-dust-66960921.us-east-2.aws.neon.tech/neondb'
     db.init_app(app)
 
     from .views import views
